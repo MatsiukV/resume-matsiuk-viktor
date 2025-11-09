9b20573a85c5abfe4277bf81ad5151c0dd9b918b
MatsiukV <matsiukviktor2003@gmail.com>
2025-11-10 00:01:25 +0200

    Update header: add burger-menu and device tracing, Create new pages: skills, projects and contact

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,26 +1,46 @@
-'use client';
+'use client'; 
 
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import Link from 'next/link';
 import './styleHeader.css';
 
 const Header: React.FC = () => {
   const [language, setLanguage] = useState('UA');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleLanguage = () => {
     setLanguage((prevLang) => (prevLang === 'UA' ? 'UK' : 'UA'));
+  };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const handleLinkClick = () => {
+    setIsMenuOpen(false);
   };
 
   return (
     <header className="header-container">
-      <nav className="header-nav">
-        <Link href="#">Про мене</Link>
-        <Link href="#skills">Навички</Link>
-        <Link href="#projects">Проекти</Link>
-        <Link href="#contact">Контакти</Link>
+      <nav className={`header-nav ${isMenuOpen ? 'mobile-open' : ''}`}>
+        <Link href="/" onClick={handleLinkClick}>Про мене</Link>
+        <Link href="/skils" onClick={handleLinkClick}>Навички</Link>
+        <Link href="/projects" onClick={handleLinkClick}>Проекти</Link>
+        <Link href="/contact" onClick={handleLinkClick}>Контакти</Link>
+        <div 
+          className="lang-switcher lang-switcher-mobile" 
+          onClick={toggleLanguage}>
+          {language}
+        </div>
       </nav>
-      <div className="lang-switcher" onClick={toggleLanguage}>
+      <div 
+        className="lang-switcher lang-switcher-desktop" 
+        onClick={toggleLanguage}>
         {language}
       </div>
+      <div className="burger-menu" onClick={toggleMenu}>
+        <div className={`burger-line ${isMenuOpen ? 'open' : ''}`}></div>
+        <div className={`burger-line ${isMenuOpen ? 'open' : ''}`}></div>
+        <div className={`burger-line ${isMenuOpen ? 'open' : ''}`}></div>
+      </div>
+
     </header>
   );
 };
